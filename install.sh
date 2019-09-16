ec28025be43ff307f1aaeabab07e2a2568a140fb
mike.basinger@gmail.com <mike.basinger@gmail.com>
2019-09-16 10:58:42 -0600

    Update script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Destination Directories
+mv mojave /usr/share/backgrounds
+chown -R root.root /usr/share/backgrounds/mojave
+chmod -R 755 /usr/share/backgrounds/mojave
+chmod 644 /usr/share/backgrounds/mojave/mojave.xml
 
+mv desktop-backgrounds-mojave.xml /usr/share/gnome-background-properties
+chmod 644 /usr/share/gnome-background-properties/desktop-backgrounds-mojave.xml
