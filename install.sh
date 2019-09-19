8079b0ae3e5c4e68d0df1830a8df14129c81b23c
Mike Basinger <mike.basinger@gmail.com>
2019-09-19 07:22:09 -0600

    brown bag script error

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mv mojave /usr/share/backgrounds
+cp -r mojave /usr/share/backgrounds
 chown -R root.root /usr/share/backgrounds/mojave
 chmod -R 755 /usr/share/backgrounds/mojave
 chmod 644 /usr/share/backgrounds/mojave/mojave.xml
 
-mv desktop-backgrounds-mojave.xml /usr/share/gnome-background-properties
+cp desktop-backgrounds-mojave.xml /usr/share/gnome-background-properties
 chmod 644 /usr/share/gnome-background-properties/desktop-backgrounds-mojave.xml
